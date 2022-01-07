53175216469997e9f826821763b500e7aaeaf916
Richard Hertrich <richy@localhost.localdomain>
2022-01-07 14:29:37 +0100

    send position to NTRIP Caster every 2s

--- MAVProxy/modules/lib/ntrip.py
+++ MAVProxy/modules/lib/ntrip.py
@@ -29,20 +29,21 @@ class NtripError(Exception):
 
 
 class NtripClient(object):
-    def __init__(self,
-                 user="",
-                 port=2101,
-                 caster="",
-                 mountpoint="",
-                 host=False,
-                 lat=46,
-                 lon=122,
-                 height=1212,
-                 ssl=False,
-                 V2=False,
-                 ):
+    def __init__(
+        self,
+        user="",
+        port=2101,
+        caster="",
+        mountpoint="",
+        host=False,
+        lat=46,
+        lon=122,
+        height=1212,
+        ssl=False,
+        V2=False,
+    ):
         if sys.version_info.major >= 3:
-            user = bytearray(user, 'ascii')
+            user = bytearray(user, "ascii")
         self.user = base64.b64encode(user)
         self.port = port
         self.caster = caster
@@ -60,6 +61,7 @@ class NtripClient(object):
         # RTCM3 parser
         self.rtcm3 = rtcm3.RTCM3()
         self.last_id = None
+        self.dt_last_gga_sent = 0
         if self.port == 443:
             # force SSL on port 443
             self.ssl = True
@@ -68,30 +70,32 @@ class NtripClient(object):
         self.flagN = "N"
         self.flagE = "E"
         if lon > 180:
-            lon = (lon-360)*-1
+            lon = (lon - 360) * -1
             self.flagE = "W"
         elif lon < 0 and lon >= -180:
-            lon = lon*-1
+            lon = lon * -1
             self.flagE = "W"
         elif lon < -180:
-            lon = lon+360
+            lon = lon + 360
             self.flagE = "E"
         else:
             self.lon = lon
         if lat < 0:
-            lat = lat*-1
+            lat = lat * -1
             self.flagN = "S"
         self.lonDeg = int(lon)
         self.latDeg = int(lat)
-        self.lonMin = (lon-self.lonDeg)*60
-        self.latMin = (lat-self.latDeg)*60
+        self.lonMin = (lon - self.lonDeg) * 60
+        self.latMin = (lat - self.latDeg) * 60
 
     def getMountPointString(self):
         userstr = self.user
         if sys.version_info.major >= 3:
-            userstr = str(userstr, 'ascii')
-        mountPointString = "GET %s HTTP/1.0\r\nUser-Agent: %s\r\nAuthorization: Basic %s\r\n" % (
-            self.mountpoint, useragent, userstr)
+            userstr = str(userstr, "ascii")
+        mountPointString = (
+            "GET %s HTTP/1.0\r\nUser-Agent: %s\r\nAuthorization: Basic %s\r\n"
+            % (self.mountpoint, useragent, userstr)
+        )
         if self.host or self.V2:
             hostString = "Host: %s:%i\r\n" % (self.caster, self.port)
             mountPointString += hostString
@@ -103,8 +107,17 @@ class NtripClient(object):
     def getGGAString(self):
         now = datetime.datetime.utcnow()
         ggaString = "GPGGA,%02d%02d%04.2f,%02d%011.8f,%1s,%03d%011.8f,%1s,1,05,0.19,+00400,M,%5.3f,M,," % (
-                     now.hour, now.minute, now.second, self.latDeg, self.latMin, self.flagN,
-                     self.lonDeg, self.lonMin, self.flagE, self.height)
+            now.hour,
+            now.minute,
+            now.second,
+            self.latDeg,
+            self.latMin,
+            self.flagN,
+            self.lonDeg,
+            self.lonMin,
+            self.flagE,
+            self.height,
+        )
         checksum = self.calculateCheckSum(ggaString)
         return "$%s*%s\r\n" % (ggaString, checksum)
 
@@ -115,7 +128,7 @@ class NtripClient(object):
         return "%02X" % xsum_calc
 
     def get_ID(self):
-        '''get ID of last packet'''
+        """get ID of last packet"""
         return self.last_id
 
     def read(self):
@@ -130,7 +143,7 @@ class NtripClient(object):
                 time.sleep(0.1)
                 mps = self.getMountPointString()
                 if sys.version_info.major >= 3:
-                    mps = bytearray(mps, 'ascii')
+                    mps = bytearray(mps, "ascii")
                 try:
                     self.socket.sendall(mps)
                 except Exception:
@@ -139,14 +152,14 @@ class NtripClient(object):
             try:
                 casterResponse = self.socket.recv(4096)
             except ssl.SSLWantReadError:
-                    return None
+                return None
             except IOError as e:
                 if e.errno == errno.EWOULDBLOCK:
                     return None
                 self.socket = None
-                casterResponse = ''
+                casterResponse = ""
             if sys.version_info.major >= 3:
-                casterResponse = str(casterResponse, 'ascii')
+                casterResponse = str(casterResponse, "ascii")
             header_lines = casterResponse.split("\r\n")
             for line in header_lines:
                 if line == "":
@@ -159,21 +172,14 @@ class NtripClient(object):
                     raise NtripError("Mount Point does not exist")
                 elif line.find(" 200 OK") != -1:
                     # Request was valid
-                    try:
-                        gga = self.getGGAString()
-                        if sys.version_info.major >= 3:
-                            gga = bytearray(gga, 'ascii')
-                        self.socket.sendall(gga)
-                    except Exception:
-                        self.socket = None
-                        return None
+                    self.send_gga()
             return None
         # normal data read
         while True:
             try:
                 data = self.socket.recv(1)
             except ssl.SSLWantReadError:
-                    return None
+                return None
             except IOError as e:
                 if e.errno == errno.EWOULDBLOCK:
                     return None
@@ -193,7 +199,7 @@ class NtripClient(object):
                 return self.rtcm3.get_packet()
 
     def connect(self):
-        '''connect to NTRIP server'''
+        """connect to NTRIP server"""
         self.sent_header = False
         self.found_header = False
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -217,60 +223,140 @@ class NtripClient(object):
                 continue
             print("got: ", len(data))
 
-if __name__ == '__main__':
+    def send_gga(self):
+        gga = self.getGGAString()
+        if sys.version_info.major >= 3:
+            gga = bytearray(gga, "ascii")
+        try:
+            self.socket.sendall(gga)
+            self.dt_last_gga_sent = time.time()
+        except Exception:
+            self.socket = None
+
+
+if __name__ == "__main__":
     usage = "NtripClient.py [options] [caster] [port] mountpoint"
     parser = OptionParser(version=version, usage=usage)
-    parser.add_option("-u", "--user", type="string", dest="user", default="IBS", help="The Ntripcaster username.  Default: %default")
-    parser.add_option("-p", "--password", type="string", dest="password", default="IBS", help="The Ntripcaster password. Default: %default")
-    parser.add_option("-o", "--org", type="string", dest="org", help="Use IBSS and the provided organization for the user. Caster and Port are not needed in this case Default: %default")
-    parser.add_option("-b", "--baseorg", type="string", dest="baseorg", help="The org that the base is in. IBSS Only, assumed to be the user org")
-    parser.add_option("-t", "--latitude", type="float", dest="lat", default=50.09, help="Your latitude.  Default: %default")
-    parser.add_option("-g", "--longitude", type="float", dest="lon", default=8.66, help="Your longitude.  Default: %default")
-    parser.add_option("-e", "--height", type="float", dest="height", default=1200, help="Your ellipsoid height.  Default: %default")
-    parser.add_option("-s", "--ssl", action="store_true", dest="ssl", default=False, help="Use SSL for the connection")
-    parser.add_option("-H", "--host", action="store_true", dest="host", default=False, help="Include host header, should be on for IBSS")
-    parser.add_option("-2", "--V2", action="store_true", dest="V2", default=False, help="Make a NTRIP V2 Connection")
+    parser.add_option(
+        "-u",
+        "--user",
+        type="string",
+        dest="user",
+        default="IBS",
+        help="The Ntripcaster username.  Default: %default",
+    )
+    parser.add_option(
+        "-p",
+        "--password",
+        type="string",
+        dest="password",
+        default="IBS",
+        help="The Ntripcaster password. Default: %default",
+    )
+    parser.add_option(
+        "-o",
+        "--org",
+        type="string",
+        dest="org",
+        help="Use IBSS and the provided organization for the user. Caster and Port are not needed in this case Default: %default",
+    )
+    parser.add_option(
+        "-b",
+        "--baseorg",
+        type="string",
+        dest="baseorg",
+        help="The org that the base is in. IBSS Only, assumed to be the user org",
+    )
+    parser.add_option(
+        "-t",
+        "--latitude",
+        type="float",
+        dest="lat",
+        default=50.09,
+        help="Your latitude.  Default: %default",
+    )
+    parser.add_option(
+        "-g",
+        "--longitude",
+        type="float",
+        dest="lon",
+        default=8.66,
+        help="Your longitude.  Default: %default",
+    )
+    parser.add_option(
+        "-e",
+        "--height",
+        type="float",
+        dest="height",
+        default=1200,
+        help="Your ellipsoid height.  Default: %default",
+    )
+    parser.add_option(
+        "-s",
+        "--ssl",
+        action="store_true",
+        dest="ssl",
+        default=False,
+        help="Use SSL for the connection",
+    )
+    parser.add_option(
+        "-H",
+        "--host",
+        action="store_true",
+        dest="host",
+        default=False,
+        help="Include host header, should be on for IBSS",
+    )
+    parser.add_option(
+        "-2",
+        "--V2",
+        action="store_true",
+        dest="V2",
+        default=False,
+        help="Make a NTRIP V2 Connection",
+    )
 
     (options, args) = parser.parse_args()
     ntripArgs = {}
 
-    ntripArgs['lat'] = options.lat
-    ntripArgs['lon'] = options.lon
-    ntripArgs['height'] = options.height
-    ntripArgs['host'] = options.host
+    ntripArgs["lat"] = options.lat
+    ntripArgs["lon"] = options.lon
+    ntripArgs["height"] = options.height
+    ntripArgs["host"] = options.host
 
     if options.ssl:
         import ssl
-        ntripArgs['ssl'] = True
+
+        ntripArgs["ssl"] = True
     else:
-        ntripArgs['ssl'] = False
+        ntripArgs["ssl"] = False
 
     if options.org:
         if len(args) != 1:
             print("Incorrect number of arguments for IBSS\n")
             parser.print_help()
             sys.exit(1)
-        ntripArgs['user'] = options.user+"." + options.org + ":" + options.password
+        ntripArgs["user"] = options.user + "." + options.org + ":" + options.password
         if options.baseorg:
-            ntripArgs['caster'] = options.baseorg + ".ibss.trimbleos.com"
+            ntripArgs["caster"] = options.baseorg + ".ibss.trimbleos.com"
         else:
-            ntripArgs['caster'] = options.org + ".ibss.trimbleos.com"
+            ntripArgs["caster"] = options.org + ".ibss.trimbleos.com"
         if options.ssl:
-            ntripArgs['port'] = 52101
+            ntripArgs["port"] = 52101
         else:
-            ntripArgs['port'] = 2101
-        ntripArgs['mountpoint'] = args[0]
+            ntripArgs["port"] = 2101
+        ntripArgs["mountpoint"] = args[0]
     else:
         if len(args) != 3:
             print("Incorrect number of arguments for NTRIP\n")
             parser.print_help()
             sys.exit(1)
-        ntripArgs['user'] = options.user+":"+options.password
-        ntripArgs['caster'] = args[0]
-        ntripArgs['port'] = int(args[1])
-        ntripArgs['mountpoint'] = args[2]
+        ntripArgs["user"] = options.user + ":" + options.password
+        ntripArgs["caster"] = args[0]
+        ntripArgs["port"] = int(args[1])
+        ntripArgs["mountpoint"] = args[2]
 
-    ntripArgs['V2'] = options.V2
+    ntripArgs["V2"] = options.V2
 
     n = NtripClient(**ntripArgs)
     n.readLoop()
--- MAVProxy/modules/mavproxy_ntrip.py
+++ MAVProxy/modules/mavproxy_ntrip.py
@@ -10,25 +10,27 @@ from MAVProxy.modules.lib import mp_settings
 
 
 class NtripModule(mp_module.MPModule):
-
     def __init__(self, mpstate):
         super(NtripModule, self).__init__(mpstate, "ntrip", "ntrip", public=False)
         self.ntrip_settings = mp_settings.MPSettings(
-            [('caster', str, None),
-             ('port', int, 2101),
-             ('username', str, 'IBS'),
-             ('password', str, 'IBS'),
-             ('mountpoint', str, None),
-             ('logfile', str, None),
-             ('sendalllinks', bool, False),
-             ('sendmul', int, 1)])
-        self.add_command('ntrip', self.cmd_ntrip, 'NTRIP control',
-                         ["<status>",
-                          "<start>",
-                          "<stop>",
-                          "set (NTRIPSETTING)"])
-        self.add_completion_function('(NTRIPSETTING)',
-                                     self.ntrip_settings.completion)
+            [
+                ("caster", str, None),
+                ("port", int, 2101),
+                ("username", str, "IBS"),
+                ("password", str, "IBS"),
+                ("mountpoint", str, None),
+                ("logfile", str, None),
+                ("sendalllinks", bool, False),
+                ("sendmul", int, 1),
+            ]
+        )
+        self.add_command(
+            "ntrip",
+            self.cmd_ntrip,
+            "NTRIP control",
+            ["<status>", "<start>", "<stop>", "set (NTRIPSETTING)"],
+        )
+        self.add_completion_function("(NTRIPSETTING)", self.ntrip_settings.completion)
         self.pos = None
         self.pkt_count = 0
         self.last_pkt = None
@@ -43,37 +45,45 @@ class NtripModule(mp_module.MPModule):
         self.last_by_id = {}
 
     def mavlink_packet(self, msg):
-        '''handle an incoming mavlink packet'''
-        if msg.get_type() in ['GPS_RAW_INT', 'GPS2_RAW']:
+        """handle an incoming mavlink packet"""
+        if msg.get_type() in ["GPS_RAW_INT", "GPS2_RAW"]:
             if msg.fix_type >= 3:
-                self.pos = (msg.lat*1.0e-7, msg.lon*1.0e-7, msg.alt*1.0e-3)
+                self.pos = (msg.lat * 1.0e-7, msg.lon * 1.0e-7, msg.alt * 1.0e-3)
 
     def log_rtcm(self, data):
-        '''optionally log rtcm data'''
+        """optionally log rtcm data"""
         if self.ntrip_settings.logfile is None:
             return
         if self.logfile is None:
-            self.logfile = open(self.ntrip_settings.logfile, 'wb')
+            self.logfile = open(self.ntrip_settings.logfile, "wb")
         if self.logfile is not None:
             self.logfile.write(data)
 
     def idle_task(self):
-        '''called on idle'''
+        """called on idle"""
         if self.start_pending and self.ntrip is None and self.pos is not None:
             self.cmd_start()
         if self.ntrip is None:
             return
         data = self.ntrip.read()
+
         if data is None:
             now = time.time()
-            if (self.last_pkt is not None and
-                now - self.last_pkt > 15 and
-                (self.last_restart is None or now - self.last_restart > 30)):
+            if (
+                self.last_pkt is not None
+                and now - self.last_pkt > 15
+                and (self.last_restart is None or now - self.last_restart > 30)
+            ):
                 print("NTRIP restart")
                 self.ntrip = None
                 self.start_pending = True
                 self.last_restart = now
             return
+
+        if time.time() - self.ntrip.dt_last_gga_sent > 2:
+            self.ntrip.setPosition(self.pos[0], self.pos[1])
+            self.ntrip.send_gga()
+
         self.log_rtcm(data)
 
         rtcm_id = self.ntrip.get_ID()
@@ -83,14 +93,14 @@ class NtripModule(mp_module.MPModule):
         self.id_counts[rtcm_id] += 1
 
         blen = len(data)
-        if blen > 4*180:
+        if blen > 4 * 180:
             # can't send this with GPS_RTCM_DATA
             return
         total_len = blen
         self.rate_total += blen * self.ntrip_settings.sendmul
 
         if blen > 180:
-            flags = 1 # fragmented
+            flags = 1  # fragmented
         else:
             flags = 0
         # add in the sequence number
@@ -102,14 +112,16 @@ class NtripModule(mp_module.MPModule):
             frag_len = len(send_data)
             data = data[frag_len:]
             if frag_len < 180:
-                send_data.extend(bytearray([0]*(180-frag_len)))
+                send_data.extend(bytearray([0] * (180 - frag_len)))
             if self.ntrip_settings.sendalllinks:
                 links = self.mpstate.mav_master
             else:
                 links = [self.master]
             for link in links:
                 for d in range(self.ntrip_settings.sendmul):
-                    link.mav.gps_rtcm_data_send(flags | (fragment<<1), frag_len, send_data)
+                    link.mav.gps_rtcm_data_send(
+                        flags | (fragment << 1), frag_len, send_data
+                    )
             fragment += 1
             blen -= frag_len
         self.pkt_count += 1
@@ -124,7 +136,7 @@ class NtripModule(mp_module.MPModule):
         self.last_pkt = now
 
     def cmd_ntrip(self, args):
-        '''ntrip command handling'''
+        """ntrip command handling"""
         if len(args) <= 0:
             print("Usage: ntrip <start|stop|status|set>")
             return
@@ -139,7 +151,7 @@ class NtripModule(mp_module.MPModule):
             self.ntrip_settings.command(args[1:])
 
     def ntrip_status(self):
-        '''show ntrip status'''
+        """show ntrip status"""
         now = time.time()
         if self.ntrip is None:
             print("ntrip: Not started")
@@ -149,12 +161,17 @@ class NtripModule(mp_module.MPModule):
             return
         frame_size = 0
         for id in sorted(self.id_counts.keys()):
-            print(" %4u: %u (len %u)" % (id, self.id_counts[id], len(self.last_by_id[id])))
+            print(
+                " %4u: %u (len %u)" % (id, self.id_counts[id], len(self.last_by_id[id]))
+            )
             frame_size += len(self.last_by_id[id])
-        print("ntrip: %u packets, %.1f bytes/sec last %.1fs ago framesize %u" % (self.pkt_count, self.rate, now - self.last_pkt, frame_size))
+        print(
+            "ntrip: %u packets, %.1f bytes/sec last %.1fs ago framesize %u"
+            % (self.pkt_count, self.rate, now - self.last_pkt, frame_size)
+        )
 
     def cmd_start(self):
-        '''start ntrip link'''
+        """start ntrip link"""
         if self.ntrip_settings.caster is None:
             print("Require caster")
             return
@@ -166,13 +183,15 @@ class NtripModule(mp_module.MPModule):
             self.start_pending = True
             return
         user = self.ntrip_settings.username + ":" + self.ntrip_settings.password
-        self.ntrip = ntrip.NtripClient(user=user,
-                                       port=self.ntrip_settings.port,
-                                       caster=self.ntrip_settings.caster,
-                                       mountpoint=self.ntrip_settings.mountpoint,
-                                       lat=self.pos[0],
-                                       lon=self.pos[1],
-                                       height=self.pos[2])
+        self.ntrip = ntrip.NtripClient(
+            user=user,
+            port=self.ntrip_settings.port,
+            caster=self.ntrip_settings.caster,
+            mountpoint=self.ntrip_settings.mountpoint,
+            lat=self.pos[0],
+            lon=self.pos[1],
+            height=self.pos[2],
+        )
         print("NTRIP started")
         self.start_pending = False
         self.last_rate = time.time()
@@ -180,5 +199,5 @@ class NtripModule(mp_module.MPModule):
 
 
 def init(mpstate):
-    '''initialise module'''
+    """initialise module"""
     return NtripModule(mpstate)
